f845851dcf408f2bcf3d5198b1b3f8a3eddf2a1e
Daksh Shah <dakshah@ucsc.edu>
2023-11-14 18:01:05 -0800

    Fixed bug with box overlay

--- Visualize.py
+++ Visualize.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 import os
 from collections import namedtuple
+from pynput import Key, Listener
 
 image_dir = os.getcwd() + "/kitti/images/"    # relative directory to images
 GT_dir = os.getcwd() + "/kitti/labels/"             # relative directory to ground truth data
@@ -13,6 +14,20 @@ min_IoU_thres = 0.5             # minumum IoU threshold
 Detection = namedtuple("Detection", ["image_name", "label", "gt", "pred"])
 Coordinates = namedtuple("Coordinates", ["label", "coords"])
 
+def on_press(key):
+    print('{0} pressed'.format(
+        key))
+
+def on_release(key):
+    print('{0} release'.format(
+        key))
+    if key == Key.e:
+        
+    elif key == key.q:
+        
+    elif key == key.c:
+        # Stop listener
+        return False
 
 def image_name(image_number):
     file_name = str(image_number)
@@ -20,32 +35,32 @@ def image_name(image_number):
 
 def overlay_box(img, coords, color):
     x1 = int(coords[0])
-    x2 = int(coords[1])
-    y1 = int(coords[2])
+    y1 = int(coords[1])
+    x2 = int(coords[2])
     y2 = int(coords[3])
     return cv2.rectangle(img, (x1, y1), (x2, y2), color=color, thickness=3)
 
 def calculate_IoU(boxA, boxB):
     # Function taken from here:
     # https://pyimagesearch.com/2016/11/07/intersection-over-union-iou-for-object-detection/
-    
+
     # determine the (x, y)-coordinates of the intersection rectangle
-	xA = max(boxA[0], boxB[0])
-	yA = max(boxA[1], boxB[1])
-	xB = min(boxA[2], boxB[2])
-	yB = min(boxA[3], boxB[3])
-	# compute the area of intersection rectangle
-	interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
-	# compute the area of both the prediction and ground-truth
-	# rectangles
-	boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
-	boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
-	# compute the intersection over union by taking the intersection
-	# area and dividing it by the sum of prediction + ground-truth
-	# areas - the interesection area
-	iou = interArea / float(boxAArea + boxBArea - interArea)
-	# return the intersection over union value
-	return iou
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+    # return the intersection over union value
+    return iou
     
 # to do
 def validate_data(outfile, label, x1, y1, x2, y2):
@@ -85,16 +100,16 @@ def display_image(infile):
         img_prototype = overlay_box(img, x.coords, HL_color)
         print(x.coords)
         cv2.imshow(image_dir + infile + ".png", img_prototype)
-        if((cv2.waitKey(0) & 0xFF) == ord('q')):
-            # if q, then approve
+        if(pt.read_key() == "e"):
+            # if e, then approve
             # to do
             print("Approve")
             # `````````````````````` insert function call to validate_coordinate
-            # cv2.destroyAllWindows()
-        elif((cv2.waitKey(0) & 0xFF) == ord('e')):
+            cv2.destroyAllWindows()
+        elif(pt.read_key() == "q"):
             # if q, then deny
             print("Deny")
-    cv2.destroyAllWindows()
+            cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
